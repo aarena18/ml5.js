a66048f08a88bfc6e6ebc04c5605dd0bd9b5f580
aarena18 <ambrearena@gmail.com>
2023-03-20 16:20:26 +0100

    :hammer: started making the game

--- ml5/sketch.js
+++ ml5/sketch.js
@@ -2,7 +2,8 @@
 //start video
 let video;
 let classifier;
-let label = "waiting...";
+let label = "inazuma...".results;
+let computerChoice;
 
 //get my model
 function preload() {
@@ -15,21 +16,28 @@ function setup() {
   createCanvas(640, 480);
   video = createCapture(VIDEO);
   video.hide();
-  classifyVideo();
 }
 
-//classify video
-function classifyVideo() {
-  classifier.classify(video, gotResults);
-}
+
+
 
 function draw() {
   background(0);
   image(video, 0, 0);
+
+  classifier.classify(video, gotResults);
+
   textSize(24);
   fill(255);
   textAlign(CENTER, CENTER);
   text(label, width / 2, height - 16);
+
+  if (computerChoice) {
+    textSize(24);
+    fill(255);
+    textAlign(CENTER, CENTER);
+    text(computerChoice, width / 2, 50);
+  }
 }
 
 //get results
@@ -40,10 +48,38 @@ function gotResults(error, results) {
   }
   //console.log(results);
   label = results[0].label;
-  classifyVideo();
 }
 
-// MACHINE PLAYER
-//
+
+function modelLoaded() {
+  console.log('Model Loaded!');
+}
 
 
+//COMPUTER
+function play() {
+  // Generate a random move for the computer
+  let choice = ['zero', 'ONE', 'TWO'];
+  computerChoice = choice[Math.floor(Math.random() * choice.length)];
+
+  // Display the computer's move
+  document.getElementById('computer').innerHTML = computerChoice;
+
+  // Compare the computer's move to the player's move
+  let result = "";
+
+  if (label === 'ONE' && computerMove === 'ONE') {
+    result = 'You win!';
+  } else if (label === 'TWO' && computerMove === 'TWO') {
+    result = 'You win!';
+  } else if (label === 'zero' && computerMove === 'zero') {
+    result = 'You win!';
+  } else {
+    result = 'you lose!';
+  }
+
+  // Display the result
+  document.getElementById('result').innerHTML = result;
+
+
+}
