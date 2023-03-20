995d54a1c4b82cac3f2f484a5979719107a524fd
aarena18 <ambrearena@gmail.com>
2023-03-20 16:46:29 +0100

    :lipstick: added style sheet

--- ml5/sketch.js
+++ ml5/sketch.js
@@ -1,11 +1,36 @@
+//COMPUTER
+function play() {
+  // Generate a random move for the computer
+  let choice = ["zero", "ONE", "TWO"];
+  computerChoice = choice[Math.floor(Math.random() * choice.length)];
+
+  document.getElementById("computer").innerHTML = computerChoice;
+
+  // Compare the computer's move to the your move
+  let result = "";
+
+  if (label === "ONE" && computerMove === "ONE") {
+    result = "You win!";
+  } else if (label === "TWO" && computerMove === "TWO") {
+    result = "You win!";
+  } else if (label === "zero" && computerMove === "zero") {
+    result = "You win!";
+  } else {
+    result = "you lose!";
+  }
+
+  // Display the result
+  document.getElementById("result").innerHTML = result;
+}
+
 // PLAYER
-//start video
+// Start video
 let video;
 let classifier;
-let label = "inazuma...".results;
+let label = "inazuma...";
 let computerChoice;
 
-//get my model
+// Get my model
 function preload() {
   classifier = ml5.imageClassifier(
     "https://teachablemachine.withgoogle.com/models/BZvPAcnTY/"
@@ -17,9 +42,6 @@ function setup() {
   video = createCapture(VIDEO);
   video.hide();
 }
-
-
-
 
 function draw() {
   background(0);
@@ -40,46 +62,12 @@ function draw() {
   }
 }
 
-//get results
+// Get results
 function gotResults(error, results) {
   if (error) {
     console.error(error);
     return;
   }
-  //console.log(results);
+  // console.log(results);
   label = results[0].label;
-}
-
-
-function modelLoaded() {
-  console.log('Model Loaded!');
-}
-
-
-//COMPUTER
-function play() {
-  // Generate a random move for the computer
-  let choice = ['zero', 'ONE', 'TWO'];
-  computerChoice = choice[Math.floor(Math.random() * choice.length)];
-
-  // Display the computer's move
-  document.getElementById('computer').innerHTML = computerChoice;
-
-  // Compare the computer's move to the player's move
-  let result = "";
-
-  if (label === 'ONE' && computerMove === 'ONE') {
-    result = 'You win!';
-  } else if (label === 'TWO' && computerMove === 'TWO') {
-    result = 'You win!';
-  } else if (label === 'zero' && computerMove === 'zero') {
-    result = 'You win!';
-  } else {
-    result = 'you lose!';
-  }
-
-  // Display the result
-  document.getElementById('result').innerHTML = result;
-
-
 }
